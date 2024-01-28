ee2ecd4f5defc57c673c49e6ed0d0ce236a6d51e
mrznaboya <zuyax@yahoo.com>
2024-01-28 15:38:29 +0800

    Update two.tsx

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,14 +1,51 @@
-import { StyleSheet } from 'react-native';
+import { Ionicons } from "@expo/vector-icons";
+import {
+  FlatList,
+  Image,
+  ListRenderItem,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import useCartStore from "@/store/cartStore";
+import { Product } from "@/store/interfaces";
 
 export default function TabTwoScreen() {
+  const { reduceProduct, addProduct, products } = useCartStore();
+
+  const renderItem: ListRenderItem<Product & { quantity: number }> = ({
+    item,
+  }) => (
+    <View style={styles.cartItemContainer}>
+      <Image style={styles.cartItemImage} source={{ uri: item.image }} />
+      <View style={styles.itemContainer}>
+        <Text style={styles.cartItemName}>{item.title}</Text>
+        <Text>Price: ${item.price}</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={{ padding: 10 }}
+          onPress={() => reduceProduct(item)}
+        >
+          <Ionicons name="remove" size={20} color="#000" />
+        </TouchableOpacity>
+
+        <Text>{item.quantity}</Text>
+        <TouchableOpacity
+          style={{ padding: 10 }}
+          onPress={() => addProduct(item)}
+        >
+          <Ionicons name="add" size={20} color="#000" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <FlatList data={products} renderItem={renderItem} />
     </View>
   );
 }
@@ -16,16 +53,28 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+
+  cartItemContainer: {
+    marginBottom: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 20,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+
+  cartItemImage: {
+    width: 50,
+    height: 50,
+  },
+  itemContainer: {
+    flex: 1,
+  },
+  cartItemName: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    alignItems: "center",
   },
 });
