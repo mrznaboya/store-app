baa5277b60524aa39d6a82f7aa13e9882e5bb88b
mrznaboya <zuyax@yahoo.com>
2024-01-28 17:25:10 +0800

    Add store persist using MMKV + Zustand

--- store/cartStore.ts
+++ store/cartStore.ts
@@ -1,6 +1,8 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 import { Product } from "./interfaces";
+import { zustandStorage } from "./mmkv";
 
 export interface CartState {
   products: Array<Product & { quantity: number }>;
@@ -9,51 +11,59 @@ export interface CartState {
   clearCart: () => void;
   items: number;
 }
-const useCartStore = create<CartState>((set) => ({
-  products: [],
-  items: 0,
-  addProduct: (product: Product) =>
-    set((state) => {
-      state.items++;
-      const hasProduct = state.products.find((p) => p.id === product.id);
+const useCartStore = create<CartState>()(
+  persist(
+    (set, get) => ({
+      products: [],
+      items: 0,
+      addProduct: (product: Product) =>
+        set((state) => {
+          state.items++;
+          const hasProduct = state.products.find((p) => p.id === product.id);
 
-      if (hasProduct) {
-        return {
-          products: state.products.map((p) => {
-            if (p.id === product.id) {
-              return { ...p, quantity: p.quantity + 1 };
-            }
-            return p;
-          }),
-        };
-      } else {
-        return {
-          products: [...state.products, { ...product, quantity: 1 }],
-        };
-      }
-    }),
+          if (hasProduct) {
+            return {
+              products: state.products.map((p) => {
+                if (p.id === product.id) {
+                  return { ...p, quantity: p.quantity + 1 };
+                }
+                return p;
+              }),
+            };
+          } else {
+            return {
+              products: [...state.products, { ...product, quantity: 1 }],
+            };
+          }
+        }),
 
-  reduceProduct: (product: Product) =>
-    set((state) => {
-      return {
-        products: state.products
-          .map((p) => {
-            if (p.id === product.id) {
-              state.items--;
-              return { ...p, quantity: p.quantity - 1 };
-            }
-            return p;
-          })
-          .filter((p) => p.quantity > 0),
-      };
+      reduceProduct: (product: Product) =>
+        set((state) => {
+          return {
+            products: state.products
+              .map((p) => {
+                if (p.id === product.id) {
+                  state.items--;
+                  return { ...p, quantity: p.quantity - 1 };
+                }
+                return p;
+              })
+              .filter((p) => p.quantity > 0),
+          };
+        }),
+      clearCart: () =>
+        set(() => {
+          return {
+            products: [],
+            items: 0,
+          };
+        }),
     }),
-  clearCart: () =>
-    set(() => {
-      return {
-        products: [],
-        items: 0,
-      };
-    }),
-}));
+    {
+      name: "cart-storage",
+      storage: createJSONStorage(() => zustandStorage),
+    }
+  )
+);
 
 export default useCartStore;
